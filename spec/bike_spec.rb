20d844b6f49a47669e3854d3ad2906f20489db1e
Sandrine <sandrine.zhangferron@gmail.com>
2014-09-15 17:05:30 +0100

    Corrected typo in bike_spec

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,3 +1,6 @@
+# link to the Bike class
+require 'bike'
+
 # we're describing the functionality of a specific class, Bike
 describe Bike  do
 	#this is a specific feature (behaviour)
@@ -6,7 +9,7 @@ describe Bike  do
 		the_bike = Bike.new #initialise a new object of Bike class
 		# expect an instance of this class to have
 		# a method "broken?" that should return false
-		expect(the_bike).not_to_be_broken
+		expect(the_bike).not_to be_broken
 end
 	
 end
